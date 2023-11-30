cea6c79ac04ca39e8892d352aac19639bc392aaf
AugustRossano <144732305+AugustRossano@users.noreply.github.com>
2023-11-30 13:49:13 -0800

    dugeslfsaj

---
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/wordGetter.cs
@@ -0,0 +1,31 @@
+/*
+ *August Rossano (And the Header author)
+ *LED:11/30/23
+ *DESC:
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wordGetter : MonoBehaviour
+{
+    public DialogueDisplay text;
+    public GameObject pointer;
+    public GameObject currentWord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentWord = pointer.GetComponent<wordSelector>().closestWord;
+        print(pointer.GetComponent<wordSelector>().closestWord);
+        if (Input.GetMouseButtonDown(0))
+        {
+            text.inputUpdate(currentWord.name);
+        };
+    }
+}
