953dedef0114c711738e59db2e24b481d504fda3
Jmiller360 <jackson.miller@digipen.edu>
2023-11-30 14:05:54 -0800

    WordObjects are a thing now.

---
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/Word Selection/WordObject.cs
@@ -0,0 +1,16 @@
+/*
+ * Name: Jackson Miller
+ * Date: 11/30/23
+ * Desc: Attached to the instantiated objects to give them thier predetermined values.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Allows this ScriptableObject to be created from the Unity Editor itself.
+[CreateAssetMenu(menuName = "Dialogue/WordObject")]
+public class WordObject : ScriptableObject
+{
+    string word;
+    float value;
+    string partOfSpeech;
+}
