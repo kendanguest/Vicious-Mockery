736507c9a080d116211241c89811c1d494af7fe1
CoffeeHound32 <Vincent.Hayes@Digipen.edu>
2023-11-30 13:55:50 -0800

    Menu and credit scene with usable buttons
    Buttons are maybe usable :/
    I put the new scenes into the test world folder

---
+++ fantasy_rap_game/Assets/Scripts/Misc_Utilities/QuitGame.cs
@@ -0,0 +1,19 @@
+/* Author:Vincent Hayes
+ * LED:11/30/23
+ * DESC: Quits game when built(probably)
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+}
+
