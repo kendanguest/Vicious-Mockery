64ce9c8f011df3322bde79bdd17da9e70ecc541d
AugustRossano <144732305+AugustRossano@users.noreply.github.com>
2023-12-07 13:19:52 -0800

    dodging minigame added as prefab

--- fantasy_rap_game/Assets/Scripts/RapBattleStuff/Dodging Minigame/DamageMitigationCheck.cs
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/Dodging Minigame/DamageMitigationCheck.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class DamageMitigationCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float bpm;
+    public float beatsTillTrigger;
+    public float damageOnHit;
+    public float damageOnDodge;
+    public GameObject meter;
+    private float timeUntilHit;
+    private bool isCurrentlyDodging;
+    public GameObject fibacheHead;
+    public GameObject parent;
+
     void Start()
     {
-        
+        timeUntilHit = ( 60f/bpm) * beatsTillTrigger+(60f/bpm)/2;
     }
 
     // Update is called once per frame
     void Update()
+
     {
-        
+        if (Time.time >= timeUntilHit)
+        {
+            if (isCurrentlyDodging)
+            {
+                meter.GetComponent<pointsArrow>().points -= damageOnDodge;
+
+            }
+            else
+            {
+                meter.GetComponent<pointsArrow>().points -= damageOnHit;
+
+            };
+
+            Destroy(parent);
+        };
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isCurrentlyDodging = false;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+         isCurrentlyDodging = true;
     }
 
-
-    
 }
