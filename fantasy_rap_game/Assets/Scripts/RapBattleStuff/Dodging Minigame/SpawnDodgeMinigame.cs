3ab6d58768c5a2e86fec7aa7568fbfcca332c1ab
AugustRossano <144732305+AugustRossano@users.noreply.github.com>
2023-12-08 13:06:06 -0800

    tweaking

--- fantasy_rap_game/Assets/Scripts/RapBattleStuff/Dodging Minigame/SpawnDodgeMinigame.cs
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/Dodging Minigame/SpawnDodgeMinigame.cs
@@ -6,15 +6,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnDodgeMinigame : MonoBehaviour
 {
     public GameObject prefab;
 
-    public void startDodgeGame(int bTT, int bpm)
+    public void startDodgeGame(int bTT, int bpm, string word)
     {
         var clone = Instantiate(prefab);
         DamageMitigationCheck DMC = clone.GetComponentInChildren<DamageMitigationCheck>();
+        clone.GetComponentInChildren<Text>().text = word;   
+
         DMC.selfUpdate(bpm, bTT);
     }
 }
