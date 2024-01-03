c65cad5cb310466a8b10e55805cf0bfd59d5798a
Jmiller360 <jackson.miller@digipen.edu>
2024-01-03 13:43:03 -0800

    Optimizations and stronger word randomization.

--- fantasy_rap_game/Assets/Scripts/RapBattleStuff/Word Selection/wordRandomizer.cs
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/Word Selection/wordRandomizer.cs
@@ -41,57 +41,66 @@ public class wordRandomizer : MonoBehaviour
     public void createWord(int numWords, string POS)
     {
         // Checks if the words needed are nouns or adjectives.
-        if(POS == "Noun")
+        for (int i = 0; i < numWords; i++)
         {
-            for (int i = 0; i < numWords; i++)
+            // This loop is repeated a number of times equal to the amount called for.
+            // Generates a random float within a range onscreen.
+            float x = Random.Range(-5f, 5f);
+            float y = Random.Range(0f, 4f);
+            position.x = x;
+            position.y = y;
+            // Creates a clone with the position taken from the floats.
+            var clone = Instantiate(prefab, position, Quaternion.identity);
+            // Grabs the moveRandomlyOnBeat from a clone and modifies it.
+            moveRandomlyOnBeat clonebeat = clone.GetComponent<moveRandomlyOnBeat>();
+            clonebeat.beatsPerMinute = BPM;
+            clonebeat.speed = speed;
+            // Generates a random word and it's point value and attaches it to the word.
+            int rand = Random.Range(0, wordsN.Count);
+            string word = "";
+            float point = 0.0f;
+            if (POS == "Noun")
             {
-                // This loop is repeated a number of times equal to the amount called for.
-                // Generates a random float within a range onscreen.
-                float x = Random.Range(-5f, 5f);
-                float y = Random.Range(0f, 4f);
-                position.x = x;
-                position.y = y;
-                // Creates a clone with the position taken from the floats.
-                var clone = Instantiate(prefab, position, Quaternion.identity);
-                // Grabs the moveRandomlyOnBeat from a clone and modifies it.
-                moveRandomlyOnBeat clonebeat = clone.GetComponent<moveRandomlyOnBeat>();
-                clonebeat.beatsPerMinute = BPM;
-                clonebeat.speed = speed;
-                // Generates a random word and it's point value and attaches it to the word.
-                int rand = Random.Range(0, wordsN.Count);
-                string word = wordsN[rand];
-                float point = pointsN[rand];
-                string PSI = POS;
-                WordCustomizer obj = clone.GetComponent<WordCustomizer>();
-                obj.selfUpdate(word, point, PSI);
+                word = wordsN[rand];
+                point = pointsN[rand];
+                wordsN.RemoveAt(rand);
+                pointsN.RemoveAt(rand);
             }
-        }
-        else if (POS == "ADJ")
-        {
-            for (int i = 0; i < numWords; i++)
+            else
             {
-                // This loop is repeated a number of times equal to the amount called for.
-                // Generates a random float within a range onscreen.
-                float x = Random.Range(-5f, 5f);
-                float y = Random.Range(0f, 4f);
-                position.x = x;
-                position.y = y;
-                // Creates a clone with the position taken from the floats.
-                var clone = Instantiate(prefab, position, Quaternion.identity);
-                // Grabs the moveRandomlyOnBeat from a clone and modifies it.
-                moveRandomlyOnBeat clonebeat = clone.GetComponent<moveRandomlyOnBeat>();
-                clonebeat.beatsPerMinute = BPM;
-                clonebeat.speed = speed;
-                // Generates a random word and it's point value and attaches it to the word.
-                int rand = Random.Range(0, wordsA.Count);
-                string word = wordsA[rand];
-                float point = pointsA[rand];
-                string PSI = POS;
-                WordCustomizer obj = clone.GetComponent<WordCustomizer>();
-                obj.selfUpdate(word, point, PSI);
+                word = wordsA[rand];
+                point = pointsA[rand];
+                wordsA.RemoveAt(rand);
+                pointsA.RemoveAt(rand);
             }
-        }
+            string PSI = POS;
+            WordCustomizer obj = clone.GetComponent<WordCustomizer>();
+            obj.selfUpdate(word, point, PSI);
+            }
         // Creates the linepointer object.
         Instantiate(lineOBJ);
+        reShuffleWords();
+    }
+    public void reShuffleWords()
+    {
+        // Clears the current word list to prepare to re-apply them all again.
+        wordsN.Clear();
+        pointsN.Clear();
+        wordsA.Clear();
+        pointsA.Clear();
+        // Sorts the words into nouns and adjectives.
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (partSpeechI[i] == "Noun")
+            {
+                wordsN.Add(words[i]);
+                pointsN.Add(points[i]);
+            }
+            else if (partSpeechI[i] == "ADJ")
+            {
+                wordsA.Add(words[i]);
+                pointsA.Add(points[i]);
+            }
+        }
     }
 }
