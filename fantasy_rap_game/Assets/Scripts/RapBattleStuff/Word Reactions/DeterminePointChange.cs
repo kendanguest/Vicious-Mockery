2a05937b5f8188b2ba3431cafc348da38a1e0781
Jmiller360 <jackson.miller@digipen.edu>
2024-01-05 14:22:21 -0800

    starting to do custom texts for rhyme bonuses.

--- fantasy_rap_game/Assets/Scripts/RapBattleStuff/Word Reactions/DeterminePointChange.cs
+++ fantasy_rap_game/Assets/Scripts/RapBattleStuff/Word Reactions/DeterminePointChange.cs
@@ -13,6 +13,7 @@ public class DeterminePointChange : MonoBehaviour
     public PointsUIController controller;
     private float mod = 1f;
     private List<string> previous = new List<string>();
+    private List<int> mods = new List<int>();
     public void implementPoints(float value, string word)
     {
         // Checks if the previous ending matches with the current ending. 
@@ -22,13 +23,27 @@ public class DeterminePointChange : MonoBehaviour
             {
                 // If it does, it doubles the modifier.
                 mod += 1f;
+                mods.Add(1);
             }
-            if(previous.Count > 1)
+            else if (previous[previous.Count - 1].Substring(previous[previous.Count - 1].Length - 1) == word.Substring(word.Length - 1))
+            {
+                // If it does with 1 letter, it adds 0.5 to the modifier.
+                mod += 0.5f;
+                mods.Add(-1);
+            }
+            if (previous.Count > 1)
             {
                 if (previous[previous.Count - 2].Substring(previous[previous.Count - 2].Length - 2) == word.Substring(word.Length - 2))
                 {
                     // If it matches with the one before the last one, it is multiplied by 1.5.
                     mod *= 1.5f;
+                    mods.Add(2);
+                }
+                else if (previous[previous.Count - 2].Substring(previous[previous.Count - 2].Length - 1) == word.Substring(word.Length - 1))
+                {
+                    // If it matches with the one before the last one, with one letter it is multiplied by 1.25.
+                    mod *= 1.25f;
+                    mods.Add(-2);
                 }
             }
         }
@@ -36,13 +51,14 @@ public class DeterminePointChange : MonoBehaviour
         if(word == "Uhhhh")
         {
             value = -3f;
+            previous.Add(word);
         }
         else
         {
             previous.Add(word);
         }
         arrow.points += (value * mod);
-        controller.displayPointChange(value * mod);
+        controller.displayPointChange(value * mod, mods, word);
         mod = 1f;
     }
 }
--- fantasy_rap_game/Assets/Scripts/Text_and_UI/PointsUI/PointsUIController.cs
+++ fantasy_rap_game/Assets/Scripts/Text_and_UI/PointsUI/PointsUIController.cs
@@ -16,12 +16,35 @@ public class PointsUIController : MonoBehaviour
     public TMP_Text gameState1;
     public TMP_Text gameState2;
     private pointsArrow points;
+    private string currentCombo;
     // Start is called before the first frame update
     void Start()
     {
         points = FindObjectOfType<pointsArrow>();
         gameState1.text = "Dead";
         gameState2.text = "even!";
+    }
+    public void displayPointChange(float points, List<int> mod, string word)
+    {
+        enemyPointsZone.text = "";
+        playerPointsZone.text = "";
+        if(points > 0)
+        {
+            currentCombo = currentCombo + "+";
+        }
+        currentCombo = currentCombo + points.ToString() + "!";
+        for(int i = 0; i < mod.Count; i++)
+        {
+            if(mod[i] > 0)
+            {
+                currentCombo = currentCombo + "\nRhyme Bonus!";
+            }
+            if (mod[i] > 1)
+            {
+                currentCombo = currentCombo + "\nx2!";
+            }
+
+        }
     }
 
 
