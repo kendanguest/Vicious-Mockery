6ffb5e7fdcc088ca4dbba02956a995ab9f90a8f9
Jmiller360 <jackson.miller@digipen.edu>
2024-02-22 14:49:58 -0800

    Fixed time.

---
+++ fantasy_rap_game/Assets/resetTimescale.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetTimescale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void resetTime()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
