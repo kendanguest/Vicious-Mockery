b0cd4bb4c47325422fe6032633acee66fc9013ba
AugustRossano <s-aurossano@lwsd.org>
2024-02-22 14:36:43 -0800

    Some Fixes and added pause hopefully

---
+++ fantasy_rap_game/Assets/stopTimeOnPause.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopTimeOnPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode button;
+    public Canvas menu;
+
+
+    void Start()
+    {
+        menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(button)&&Time.timeScale==1f)
+        {
+            Time.timeScale = 0f;
+            menu.enabled = true;
+        }
+        else if (Input.GetKeyDown(button)&&Time.timeScale==0f)
+        {
+            Time.timeScale=1f;
+            menu.enabled = false;
+            
+        }
+
+    }
+}
