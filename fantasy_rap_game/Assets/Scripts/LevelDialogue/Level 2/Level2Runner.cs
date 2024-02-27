eca58c00ded0e2fe1225836ecd17fea0d1d8f126
Jmiller360 <jackson.miller@digipen.edu>
2024-02-27 14:21:33 -0800

    Title screen and most of level 2

--- fantasy_rap_game/Assets/Scripts/LevelDialogue/Level 2/Level2Runner.cs
+++ fantasy_rap_game/Assets/Scripts/LevelDialogue/Level 2/Level2Runner.cs
@@ -6,6 +6,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -26,11 +27,15 @@ public class Level2Runner : MonoBehaviour
     public TransitionBehavior transition;
     public GenerateNote gnote;
     public MenuFunctions menuFunctions;
+    public TMP_Text getReady;
+    private int denom = 140;
+    private float beat = 60f / 140f;
     // Start is called before the first frame update
     void Start()
     {
         // This starts the Coroutine.
         SDM = FindObjectOfType<SpawnDodgeMinigame>();
+        getReady.text = "";
         UI.SetActive(false);
         dialogue.currentDialogue = prefightDialogue;
         StartCoroutine(Startlevel());
@@ -39,7 +44,6 @@ public class Level2Runner : MonoBehaviour
     IEnumerator Startlevel()
     {
         // This is exactly the same thing as done in level 1. Fun.
-        float beat = 60f / 180f;
         line = 0;
         yield return new WaitForSeconds(1f);
         dialogue.DisplayDialogue(prefightDialogue);
@@ -49,7 +53,45 @@ public class Level2Runner : MonoBehaviour
         progress = false;
         line = 0;
         dialogue.currentDialogue = trueRap;
-        StartCoroutine(SpeedControl());
+        trueRap.BPM = denom;
+        MusicController.jukeboxSwitch();
+        getReady.text = "Get Ready!";
+        yield return new WaitForSecondsRealtime(beat * 16);
+        getReady.text = "";
+        dialogue.DisplayDialogue(trueRap);
+        yield return new WaitUntil(() => line == 0);
+        newPlayerTurn(line, "Noun", false);
+        yield return new WaitUntil(() => line == 1);
+        newPlayerTurn(line, "ADJ", false);
+        yield return new WaitUntil(() => line == 2);
+        newPlayerTurn(line, "Noun", false);
+        yield return new WaitUntil(() => line == 3);
+        speedup(20);
+        newEnemyTurn(line, denom, "thrifted");
+        yield return new WaitUntil(() => line == 4);
+        newEnemyTurn(line, denom, "muck");
+        yield return new WaitUntil(() => line == 5);
+        newEnemyTurn(line, denom, "uplifted");
+        yield return new WaitUntil(() => line == 6);
+        newEnemyTurn(line, denom, "guck");
+        yield return new WaitUntil(() => line == 7);
+        speedup(20);
+        newPlayerTurn(line, "Noun", false);
+        yield return new WaitUntil(() => line == 8);
+        newPlayerTurn(line, "Noun", false);
+        yield return new WaitUntil(() => line == 9);
+        newPlayerTurn(line, "ADJ", false);
+        yield return new WaitUntil(() => line == 10);
+        newPlayerTurn(line, "ADJ", false);
+        yield return new WaitUntil(() => line == 11);
+        speedup(20);
+        newEnemyTurn(line, denom, "class");
+        yield return new WaitUntil(() => line == 12);
+        newEnemyTurn(line, denom, "hand");
+        yield return new WaitUntil(() => line == 13);
+        newEnemyTurn(line, denom, "pass");
+        yield return new WaitUntil(() => line == 14);
+        newEnemyTurn(line, denom, "command");
         yield return new WaitUntil(() => (progress == true));
         UI.SetActive(true);
         progress = false;
@@ -77,14 +119,13 @@ public class Level2Runner : MonoBehaviour
         RSC.shadowRap(line);
         SDM.startDodgeGame(bpm, bpm*3, insultWord);
     }
-    IEnumerator SpeedControl()
+    private void speedup(int am)
     {
-        // This is needed specifically for this level as the speed of this level ramps up over time.
-        MusicController.jukeboxSwitch();
-        dialogue.DisplayDialogue(trueRap);
-        yield return new WaitUntil(() => line == 0);
-        newPlayerTurn(line, "Noun", false);
-        yield return new WaitUntil(() => line == 1);
-        newEnemyTurn(line, 120, "that");
+        denom += am;
+        beat = 60f / denom;
+        random.BPM += am;
+        gnote.BPM += am;
+        trueRap.BPM += am;
     }
+    
     }
