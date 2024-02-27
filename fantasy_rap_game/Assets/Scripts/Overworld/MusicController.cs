de8d6b847eeb65297e8686ea0ac193957e7f75b5
Jmiller360 <jackson.miller@digipen.edu>
2024-02-27 13:00:11 -0800

    Pause does the pause pause pause feature now it pause :) pause :100:

--- fantasy_rap_game/Assets/Scripts/Overworld/MusicController.cs
+++ fantasy_rap_game/Assets/Scripts/Overworld/MusicController.cs
@@ -11,17 +11,39 @@ public class MusicController : MonoBehaviour
 {
     public AudioSource jukebox1;
     public AudioSource jukebox2;
+    private bool rapSection;
+    private bool paused;
 
+    void Start()
+    {
+        rapSection = false;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && rapSection && !paused)
+        {
+            jukebox2.Pause();
+            paused = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && rapSection && paused)
+        {
+            jukebox2.Play();
+            paused = false;
+        }
+
+    }
     //Switches from jukebox 1 to jukebox 2.
     public void jukeboxSwitch()
     {
         jukebox1.Stop();
         jukebox2.Play();
+        rapSection = true;
     }
     //Switches from jukebox 2 to jukebox 1.
     public void jukeboxSwitchBack()
     {
         jukebox2.Stop();
         jukebox1.Play();
+        rapSection = false;
     }
 }
